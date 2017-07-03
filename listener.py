3bdb4295ab60ed5e01312b711f2f888edb094b08
neogimp <mccann.jesse@gmail.com>
2017-07-03 00:54:36 -0700

    Create listener.py

---
+++ listener.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from roku import Roku
+app = Flask(__name__)
+
+roku = Roku('192.168.1.2') # change to the IP address of your Roku TV
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/up")
+def volup():
+    for lvls in range(5):
+        roku.volup()
+    return "louder!"
+
+@app.route("/down")
+def voldn():
+    for lvls in range(5):
+        roku.voldn()
+    return "Shhh..."
